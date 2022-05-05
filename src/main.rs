7d755d82f49c1ef2bdf54ce0bcded9cfa70bfc36
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2022-05-04 22:21:12 -0300

    initial prototyping of game logic in regards of comparing random word with guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Eq;
+use std::io;
 use std::process;
 
 use rust_wordle::CurrentGame;
@@ -9,4 +11,28 @@ fn main() {
     });
 
     println!("The current game is: {:?}", game);
+
+    loop {
+        println!("Input your word");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read guess.");
+
+        let guess: String = match guess.trim().to_lowercase().parse() {
+            Ok(str) => str,
+            Err(_) => continue,
+        };
+
+        println!(
+            "Your guess: {}. Current word: {}. It's equal? {}",
+            guess,
+            game.current_word,
+            guess.eq(&game.current_word)
+        );
+
+        break;
+    }
 }
